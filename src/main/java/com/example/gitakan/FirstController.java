a26835d9596109e60e03080d8ca2209fdf9fe345
shahen <shahen.hovsepyan@gmail.com>
2020-07-05 11:45:24 +0300

    test

--- src/main/java/com/example/gitakan/FirstController.java
+++ src/main/java/com/example/gitakan/FirstController.java
@@ -3,11 +3,8 @@ package com.example.gitakan;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-//@RequestMapping("/")
 public class FirstController {
 
 
